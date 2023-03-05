29dbb649960dc1ffe65b73ab3acc9ec4b3d05389
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-03-05 20:32:52 -0300

    Stage 3

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -1,19 +1,20 @@
 package calculator
 
-val twoNumberRegex = Regex("\\s*-?(\\d+)\\s+-?(\\d+)\\s*")
-val oneNumberRegex = Regex("\\s*-?(\\d+)\\s*")
+val isNumber = Regex("\\s*-?(\\d+)\\s*")
+val isSpace = Regex("\\s+")
 fun main() {
 
     while (true) {
         val input = readln()
+        val tokens = input.split(isSpace)
 
-        if (twoNumberRegex.matches(input)) {
-            println(input.split(" ").sumOf { it.toInt() })
-        } else if (oneNumberRegex.matches(input)) {
-            println(input.toInt())
+        if (tokens.all { it.matches(isNumber) }) {
+            println(tokens.sumOf { it.toInt() })
         } else if (input == "/exit") {
             println("Bye!")
             break
+        } else if (input == "/help") {
+            println("The program calculates the sum of numbers")
         }
     }
 }
