eb5db08c68dec18d4ef30cdc8eb673bf736ffc84
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-03-05 20:25:47 -0300

    Stage 2

--- Smart Calculator (Kotlin)/task/src/calculator/Main.kt
+++ Smart Calculator (Kotlin)/task/src/calculator/Main.kt
@@ -1,6 +1,19 @@
 package calculator
 
+val twoNumberRegex = Regex("\\s*-?(\\d+)\\s+-?(\\d+)\\s*")
+val oneNumberRegex = Regex("\\s*-?(\\d+)\\s*")
 fun main() {
-    val (a, b) = readln().split(" ").map { it.toInt() }
-    println(a + b)
+
+    while (true) {
+        val input = readln()
+
+        if (twoNumberRegex.matches(input)) {
+            println(input.split(" ").sumOf { it.toInt() })
+        } else if (oneNumberRegex.matches(input)) {
+            println(input.toInt())
+        } else if (input == "/exit") {
+            println("Bye!")
+            break
+        }
+    }
 }
